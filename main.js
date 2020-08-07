ef13b9418308e7c48e27c832d05256d274e65199
Streed <dieppois.streed@gmail.com>
2020-08-07 16:19:09 +0200

    showtime

---
+++ main.js
@@ -0,0 +1,25 @@
+const time = document.getElementById("time");
+
+//Affichage d'heure
+function showTime() {
+  let today = new Date(),
+    hour = today.getHours(),
+    min = today.getMinutes(),
+    sec = today.getSeconds(),
+    //format de l'heure
+    hour = hour % 12 || 12;
+
+  time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(
+    sec
+  )}`;
+
+  setTimeout(showTime, 1000);
+}
+
+//Add le zéros
+function addZero(n) {
+  return (parseInt(n, 10) < 10 ? "0" : "") + n;
+}
+
+//Run
+showTime();
